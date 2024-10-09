3de6171122238285bcaff90ee4778944a527d4d7
khaledmareimed <156708664+khaledmareimed@users.noreply.github.com>
2024-10-09 22:01:17 +0300

    Update gga-ssh2-wordpress-sample.php

--- wp/wp-content/plugins/5187673d1b4e1a479c48ca56632fb654-a1993be01a2c5e39099b3793582dab4a06eea278/gga-ssh2-wordpress-sample.php
+++ wp/wp-content/plugins/5187673d1b4e1a479c48ca56632fb654-a1993be01a2c5e39099b3793582dab4a06eea278/gga-ssh2-wordpress-sample.php
@@ -1,108 +1,89 @@
 <?php
 /*
-Plugin Name: Wp-ftp-media-library
-Plugin URI: http://wordpress.stackexchange.com/questions/74180/upload-images-to-remote-server
-Description: Let's you upload images to ftp-server and remove the upload on the local machine.
-Version: 0.1
-Author: Pontus Abrahamsson
-Author URI: http://pontusab.se
+Plugin Name: URL Media Importer
+Description: Add images and files to the WordPress media library using only a URL.
+Version: 1.0
+Author: Your Name
 */
 
-/**
- * @version 0.1
- */
-
-function wpse_74180_upload_to_ftp( $args ) {
-
-	$upload_dir = wp_upload_dir();
-	$upload_url = get_option('upload_url_path');
-	$upload_yrm = get_option('uploads_use_yearmonth_folders');
-
-
-	/**
-	 * Change this to match your server
-	 * You only need to change the those with (*)
-	 * If marked with (-) its optional 
-	 */
-
-	$settings = array(
-		'host'	  =>	'ftpupload.net',  			// * the ftp-server hostname
-		'port'    =>    21,                                 // * the ftp-server port (of type int)
-		'user'	  =>	'if0_37477331', 				// * ftp-user
-		'pass'	  =>	'ZNXFXOoJoQh9d',	 				// * ftp-password
-		'cdn'     =>    'ugg.free.nf',			// * This have to be a pointed domain or subdomain to the root of the uploads
-		'path'	  =>	'/home/vol1_4/infinityfree.com/if0_37477331',	 					// - ftp-path, default is root (/). Change here and add the dir on the ftp-server,
-		'base'	  =>    $upload_dir['basedir']  	// Basedir on local 
-	);
-
-
-	/**
-	 * Change the upload url to the ftp-server
-	 */
-
-	if( empty( $upload_url ) ) {
-		update_option( 'upload_url_path', esc_url( $settings['cdn'] ) );
-	}
-
-
-	/**
-	 * Host-connection
-	 * Read about it here: http://php.net/manual/en/function.ftp-connect.php
-	 */
-	
-	$connection = ftp_connect( $settings['host'], $settings['port'] );
-
-
-	/**
-	 * Login to ftp
-	 * Read about it here: http://php.net/manual/en/function.ftp-login.php
-	 */
-
-	$login = ftp_login( $connection, $settings['user'], $settings['pass'] );
-
-	
-	/**
-	 * Check ftp-connection
-	 */
-
-	if ( !$connection || !$login ) {
-	    die('Connection attempt failed, Check your settings');
-	}
-
-
-	function ftp_putAll($conn_id, $src_dir, $dst_dir, $created) {
-            $d = dir($src_dir);
-	    while($file = $d->read()) { // do this for each file in the directory
-	        if ($file != "." && $file != "..") { // to prevent an infinite loop
-	            if (is_dir($src_dir."/".$file)) { // do the following if it is a directory
-	                if (!@ftp_chdir($conn_id, $dst_dir."/".$file)) {
-	                    ftp_mkdir($conn_id, $dst_dir."/".$file); // create directories that do not yet exist
-	                }
-	                $created  = ftp_putAll($conn_id, $src_dir."/".$file, $dst_dir."/".$file, $created); // recursive part
-	            } else {
-	                $upload = ftp_put($conn_id, $dst_dir."/".$file, $src_dir."/".$file, FTP_BINARY); // put the files
-	                if($upload)
-	                	$created[] = $src_dir."/".$file;
-	            }
-	        }
-	    }
-	    $d->close();
-	    return $created;
-	}
-
-	/**
-	 * If we ftp-upload successfully, mark it for deletion
-	 * http://php.net/manual/en/function.ftp-put.php
-	 */
-	$delete = ftp_putAll($connection, $settings['base'], $settings['path'], array());
-	
-
-
-	// Delete all successfully-copied files
-	foreach ( $delete as $file ) {
-		unlink( $file );
-	}
-	
-	return $args;
+// Add menu item under Media
+function url_media_importer_menu() {
+    add_media_page(
+        'URL Media Importer',
+        'URL Media Importer',
+        'upload_files',
+        'url-media-importer',
+        'url_media_importer_page'
+    );
 }
-add_filter( 'wp_generate_attachment_metadata', 'wpse_74180_upload_to_ftp' );
+add_action('admin_menu', 'url_media_importer_menu');
+
+// Create the importer page
+function url_media_importer_page() {
+    ?>
+    <div class="wrap">
+        <h1>URL Media Importer</h1>
+        <form method="post" action="">
+            <table class="form-table">
+                <tr>
+                    <th><label for="media_url">Media URL</label></th>
+                    <td>
+                        <input type="url" name="media_url" id="media_url" class="regular-text" required>
+                        <p class="description">Enter the URL of the image or file you want to import.</p>
+                    </td>
+                </tr>
+            </table>
+            <?php submit_button('Import Media'); ?>
+        </form>
+    </div>
+    <?php
+
+    // Process form submission
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['media_url'])) {
+        $url = esc_url_raw($_POST['media_url']);
+        $result = url_media_importer_process($url);
+
+        if (is_wp_error($result)) {
+            echo '<div class="error"><p>' . $result->get_error_message() . '</p></div>';
+        } else {
+            echo '<div class="updated"><p>Media imported successfully! <a href="' . esc_url(get_edit_post_link($result)) . '">View in Media Library</a></p></div>';
+        }
+    }
+}
+
+// Process the URL and add to media library
+function url_media_importer_process($url) {
+    require_once(ABSPATH . 'wp-admin/includes/media.php');
+    require_once(ABSPATH . 'wp-admin/includes/file.php');
+    require_once(ABSPATH . 'wp-admin/includes/image.php');
+
+    // Download file to temp dir
+    $tmp = download_url($url);
+
+    if (is_wp_error($tmp)) {
+        return $tmp;
+    }
+
+    $file_array = array(
+        'name' => basename($url),
+        'tmp_name' => $tmp
+    );
+
+    // Check file type
+    $file_type = wp_check_filetype(basename($url), null);
+    if (!$file_type['type']) {
+        unlink($tmp);
+        return new WP_Error('invalid_file_type', 'The file type is not allowed.');
+    }
+
+    // Use media_handle_sideload to add file to media library
+    $id = media_handle_sideload($file_array, 0);
+
+    // Check for errors
+    if (is_wp_error($id)) {
+        @unlink($file_array['tmp_name']);
+        return $id;
+    }
+
+    return $id;
+}
