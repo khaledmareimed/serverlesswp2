d71d4203c36ad88c9b2d8658650c4cbe90cc62f0
khaledmareimed <156708664+khaledmareimed@users.noreply.github.com>
2024-10-10 01:00:35 +0300

    khaled go

---
+++ wp/wp-content/plugins/testplugin/testttt.php
@@ -0,0 +1,74 @@
+<?php
+/*
+Plugin Name: WP Image URL Media
+Description: Adds images to WordPress media library by URL without uploading the actual image to the server.
+Version: 1.0
+Author: Your Name
+*/
+
+// Hook to admin menu to add a custom page for adding image by URL
+add_action('admin_menu', 'wp_image_url_media_menu');
+
+function wp_image_url_media_menu() {
+    add_media_page(
+            'Add Image by URL', // Page title
+                    'Add Image by URL', // Menu title
+                            'upload_files',     // Capability
+                                    'add-image-url',    // Menu slug
+                                            'wp_image_url_media_page' // Callback function
+                                                );
+                                                }
+
+                                                // Callback function to render the form to input image URL
+                                                function wp_image_url_media_page() {
+                                                    ?>
+                                                        <div class="wrap">
+                                                                <h1>Add Image by URL</h1>
+                                                                        <form method="post" action="">
+                                                                                    <label for="image_url">Image URL:</label>
+                                                                                                <input type="text" name="image_url" id="image_url" style="width: 50%;" required />
+                                                                                                            <input type="submit" name="submit_image_url" class="button button-primary" value="Add Image">
+                                                                                                                    </form>
+                                                                                                                        </div>
+                                                                                                                            <?php
+
+                                                                                                                                // Handle form submission
+                                                                                                                                    if (isset($_POST['submit_image_url'])) {
+                                                                                                                                            $image_url = esc_url_raw($_POST['image_url']);
+                                                                                                                                                    wp_image_url_add_to_media_library($image_url);
+                                                                                                                                                        }
+                                                                                                                                                        }
+
+                                                                                                                                                        // Function to add the image URL to the media library
+                                                                                                                                                        function wp_image_url_add_to_media_library($image_url) {
+                                                                                                                                                            // Check if the URL is valid
+                                                                                                                                                                if (filter_var($image_url, FILTER_VALIDATE_URL)) {
+
+                                                                                                                                                                        // Set up post data to insert as attachment
+                                                                                                                                                                                $attachment_data = array(
+                                                                                                                                                                                            'guid'           => $image_url,
+                                                                                                                                                                                                        'post_mime_type' => 'image/jpeg', // Default mime type
+                                                                                                                                                                                                                    'post_title'     => sanitize_text_field(basename($image_url)),
+                                                                                                                                                                                                                                'post_content'   => '',
+                                                                                                                                                                                                                                            'post_status'    => 'inherit'
+                                                                                                                                                                                                                                                    );
+
+                                                                                                                                                                                                                                                            // Insert attachment into the database
+                                                                                                                                                                                                                                                                    $attachment_id = wp_insert_attachment($attachment_data, '', 0);
+
+                                                                                                                                                                                                                                                                            // If the attachment is inserted successfully
+                                                                                                                                                                                                                                                                                    if (!is_wp_error($attachment_id)) {
+                                                                                                                                                                                                                                                                                                // Optionally set image URL as post meta or other information
+                                                                                                                                                                                                                                                                                                            update_post_meta($attachment_id, '_wp_attached_file', $image_url);
+
+                                                                                                                                                                                                                                                                                                                        // Generate metadata (though no file exists locally)
+                                                                                                                                                                                                                                                                                                                                    wp_update_attachment_metadata($attachment_id, array());
+
+                                                                                                                                                                                                                                                                                                                                                echo '<div class="notice notice-success is-dismissible"><p>Image URL added to the media library successfully!</p></div>';
+                                                                                                                                                                                                                                                                                                                                                        } else {
+                                                                                                                                                                                                                                                                                                                                                                    echo '<div class="notice notice-error is-dismissible"><p>Failed to add image URL to the media library.</p></div>';
+                                                                                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                                                                                                } else {
+                                                                                                                                                                                                                                                                                                                                                                                        echo '<div class="notice notice-error is-dismissible"><p>Invalid image URL.</p></div>';
+                                                                                                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                                                                                                            }
