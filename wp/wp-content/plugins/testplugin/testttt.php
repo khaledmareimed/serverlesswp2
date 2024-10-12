e7d932ec39f2fcfda7bce0d18fb7b95dd13877dc
khaledmareimed <156708664+khaledmareimed@users.noreply.github.com>
2024-10-12 08:36:31 +0300

    Update testttt.php

--- wp/wp-content/plugins/testplugin/testttt.php
+++ wp/wp-content/plugins/testplugin/testttt.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: Imgbb Image Upload
- * Description: Automatically upload images to imgbb and store them in the WordPress media library.
- * Version: 1.0
+ * Description: Automatically upload images to Imgbb and store them in the WordPress media library.
+ * Version: 1.1
  * Author: Your Name
  */
 
@@ -11,26 +11,73 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-// Define the imgbb API key
-define('IMGBB_API_KEY', 'bff07ffc45c353b36a9ab606e7500647');
+// Add settings link on plugin page
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'imgbb_add_settings_link');
+
+function imgbb_add_settings_link($links) {
+    $settings_link = '<a href="options-general.php?page=imgbb-settings">Settings</a>';
+    array_push($links, $settings_link);
+    return $links;
+}
+
+// Create settings menu
+add_action('admin_menu', 'imgbb_create_menu');
+
+function imgbb_create_menu() {
+    add_options_page(
+        'Imgbb Image Upload Settings',
+        'Imgbb Image Upload',
+        'manage_options',
+        'imgbb-settings',
+        'imgbb_settings_page'
+    );
+}
+
+// Register settings
+add_action('admin_init', 'imgbb_register_settings');
+
+function imgbb_register_settings() {
+    register_setting('imgbb-settings-group', 'imgbb_api_key');
+}
+
+// Settings page HTML
+function imgbb_settings_page() {
+    ?>
+    <div class="wrap">
+        <h1>Imgbb Image Upload Settings</h1>
+        <form method="post" action="options.php">
+            <?php settings_fields('imgbb-settings-group'); ?>
+            <?php do_settings_sections('imgbb-settings-group'); ?>
+            <table class="form-table">
+                <tr valign="top">
+                    <th scope="row">Imgbb API Key</th>
+                    <td><input type="text" name="imgbb_api_key" value="<?php echo esc_attr(get_option('imgbb_api_key')); ?>" size="50" /></td>
+                </tr>
+            </table>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php
+}
 
 // Hook into the media upload process
 add_filter('wp_handle_upload', 'imgbb_upload_image_to_imgbb');
 
-/**
- * Upload image to imgbb, then replace with imgbb URL
- *
- * @param array $upload
- * @return array
- */
 function imgbb_upload_image_to_imgbb($upload) {
+    // Get the API key from settings
+    $api_key = get_option('imgbb_api_key');
+    if (empty($api_key)) {
+        return $upload; // If API key is not set, do nothing
+    }
+
     // Get the file path and read the image data
     $file = $upload['file'];
     $image_data = base64_encode(file_get_contents($file));
 
-    // Make the API request to imgbb
-    $response = wp_remote_post('https://api.imgbb.com/1/upload?key=' . IMGBB_API_KEY, [
+    // Make the API request to Imgbb
+    $response = wp_remote_post('https://api.imgbb.com/1/upload', [
         'body' => [
+            'key' => $api_key,
             'image' => $image_data,
         ],
     ]);
@@ -43,15 +90,15 @@ function imgbb_upload_image_to_imgbb($upload) {
     $response_body = wp_remote_retrieve_body($response);
     $json_response = json_decode($response_body, true);
 
-    // Check if the upload to imgbb was successful
+    // Check if the upload to Imgbb was successful
     if (isset($json_response['data']['url'])) {
-        // Retrieve the imgbb URL
-        $imgbb_url = $json_response['data']['url'];
+        // Retrieve the Imgbb URL
+        $imgbb_url = esc_url($json_response['data']['url']);
 
-        // Replace the local file URL with the imgbb URL
+        // Replace the local file URL with the Imgbb URL
         $upload['url'] = $imgbb_url;
 
-        // Delete the local file to save space (optional)
+        // Optional: Delete the local file to save space
         if (file_exists($file)) {
             unlink($file);
         }
